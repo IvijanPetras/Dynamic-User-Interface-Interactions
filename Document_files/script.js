687f73d7c141738f6aa40ca3a25d106f88e535a1
Ivijan <iapetras@hotmail.com>
2023-02-24 19:00:16 +0100

    image slider

---
+++ Document_files/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+// dropdown menu ----------------------
+    let menuIcon = document.querySelector('.menu-icon');
+    let container = document.querySelector('.drop-container');
+
+    menuIcon.addEventListener("click", () => {
+       container.classList.toggle('strech-it');
+    });
+/// mobile menu -------------------------
+
+
+
+});

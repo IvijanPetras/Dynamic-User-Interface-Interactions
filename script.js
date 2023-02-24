90d900dfda2fb52dc1d569838ad5d00941f1baba
Ivijan <iapetras@hotmail.com>
2023-02-24 05:28:03 +0100

    dropdown menu

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
-
+// dropdown menu ----------------------
     let menuIcon = document.querySelector('.menu-icon');
-    let home = document.querySelector('.home');
-    let contact = document.querySelector('.contact');
-    let about = document.querySelector('.about');
     let container = document.querySelector('.drop-container');
-
-    function animate(elem) {
-        if(elem.classList[1] === undefined || elem.classList[1] === 'dropit') {
-          elem.classList.remove("dropit");
-          elem.classList.add("dropit-reverse");
-        } else {
-        elem.classList.remove("dropit-reverse");
-        elem.classList.add("dropit");
-        }
-        
-      }
-    
 
     menuIcon.addEventListener("click", () => {
        container.classList.toggle('strech-it');
     });
+/// mobile menu -------------------------
+
+
 
 });

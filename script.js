8c364a98bef89211c0d48bbdcee10a9115ebb595
Ivijan <iapetras@hotmail.com>
2023-02-25 22:06:16 +0100

    still

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
 // dropdown menu ----------------------
-    let menuIcon = document.querySelector('.menu-icon');
-    let container = document.querySelector('.drop-container');
+    // let menuIcon = document.querySelector('.menu-icon');
+    // let container = document.querySelector('.drop-container');
 
-    menuIcon.addEventListener("click", () => {
-       container.classList.toggle('strech-it');
-    });
-/// mobile menu -------------------------
+    // menuIcon.addEventListener("click", () => {
+    //    container.classList.toggle('strech-it');
+    // });
+/// image slider -------------------------
 
+    let carousel = document.querySelector('.carousel');
+    let prevBtn = document.querySelector('.prevBtn');
+    let nextBtn = document.querySelector('.next');
 
+    
 
 });

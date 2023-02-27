e985dc8f8716a723229bbc7afbfe22449b16e159
Ivijan <iapetras@hotmail.com>
2023-02-27 12:24:06 +0100

    logic work

--- script.js
+++ script.js
@@ -9,9 +9,33 @@ window.addEventListener("DOMContentLoaded", () => {
 /// image slider -------------------------
 
     let carousel = document.querySelector('.carousel');
-    let prevBtn = document.querySelector('.prevBtn');
+    let prevBtn = document.querySelector('.prev');
     let nextBtn = document.querySelector('.next');
+    let dotContainer = document.querySelector('.dot-container');
+    let move = 1;
 
-    
+    function activeSlide(){
+       let imgs = [...carousel.children]
+       let activeImg = imgs.filter(img => img.classList.contains('active'));
+       return imgs.indexOf(...activeImg) + 1;
+    };
+
+    console.log(activeSlide())
+
+    function nextSlide(){
+
+        carousel.style.setProperty('--pics-move', `${-800 * activeSlide()}px`);
+        move++;
+       
+    };
+
+    function prevSlide(){
+        move = 0;
+        console.log(move)
+        carousel.style.setProperty('--pics-move', `${800 * move}px`);
+    };
+
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
 
 });

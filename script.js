e2a909901bdc89ca22aaa086d69a65b1f3558adc
Ivijan <iapetras@hotmail.com>
2023-03-01 09:58:50 +0100

    roughly finished

--- script.js
+++ script.js
@@ -12,31 +12,57 @@ window.addEventListener("DOMContentLoaded", () => {
     let prevBtn = document.querySelector('.prev');
     let nextBtn = document.querySelector('.next');
     let dotContainer = document.querySelector('.dot-container');
-    let move = 1;
-    let imgs = [...carousel.children]
+    let move = 0;
+    const IMAGES = [...carousel.children]
     
     
+    IMAGES.forEach((img) => {
+        let newDot = document.createElement('div');
+        newDot.classList.add('dot');
+        newDot.setAttribute('id', `${IMAGES.indexOf(img)}`);
+        dotContainer.appendChild(newDot);
+    });
+
+    function slideMovement(arg){
+        `${arg}`;
+        carousel.style.setProperty('--pics-move', `${-800 * move}px`);
+        addActiveDot();
+    }
+
     function nextSlide(){
-        const activeImg = [...carousel.children].filter(img => img.classList.contains('active'));
-        let activeImage = imgs.indexOf(...activeImg);
-        carousel.style.setProperty('--pics-move', `${-800 * (activeImage + 1)}px`);
-        activeImg[0].classList.remove('active');
-        imgs[activeImage + 1].classList.add('active');
-        console.log(imgs)
+        if(move >= IMAGES.length - 1) return
+        removeActiveDot();
+        slideMovement(move++);
     };
 
     function prevSlide(){
-        const activeImg = [...carousel.children].filter(img => img.classList.contains('active'));
-        let activeImage = imgs.indexOf(...activeImg);
-        activeImg[0].classList.remove('active');
-        imgs[activeImage - 1].classList.add('active');
-        carousel.style.setProperty('--pics-move', `${800 * (activeImage - 1)}px`);
-        console.log(activeImage);
+        if(move < 1) return 
+        removeActiveDot();
+        slideMovement(move--);
     };
 
-    nextBtn.addEventListener('click', () => {
-        nextSlide();
-    });
+    function removeActiveDot(){
+        let dotArray = Array.from(dotContainer.children);
+       if (move >= 0) dotArray.forEach(img => img.classList.remove('dot-active'));
+    }
+
+    function addActiveDot(){
+        let currentImageDot = dotContainer.children[move];
+        currentImageDot.classList.add('dot-active');
+    }
+
+    function switchImageOnDot(e){
+        let dotArray = Array.from(dotContainer.children);
+        let dotArrayIndex = dotArray.indexOf(e.target);
+        move = dotArrayIndex;
+        removeActiveDot();
+        carousel.style.setProperty('--pics-move', `${-800 * move}px`);
+        dotArray[dotArrayIndex].classList.add('dot-active');
+    }
+
+    nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
+    dotContainer.addEventListener('click', switchImageOnDot);
+    addActiveDot();
 
 });
